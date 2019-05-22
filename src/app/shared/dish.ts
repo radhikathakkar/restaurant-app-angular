23526958483f6a8c3cbc07cd84a8dc54cb22280d
Radhika <thakkarradhika1611@gmail.com>
2019-05-22 02:49:57 -0700

    angular component 2

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+export class Dish {
+    id: String;
+    name: String;
+    image: String;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+}
