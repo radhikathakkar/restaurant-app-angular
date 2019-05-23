eb1eaa1210c19552b83c52bd1a33a50ac6e4c483
Radhika <thakkarradhika1611@gmail.com>
2019-05-22 17:13:33 -0700

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,17 @@ import { from } from 'rxjs';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +33,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     FlexLayoutModule,
     MatListModule, MatGridListModule, MatCardModule, MatButtonModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/dishes.ts
+++ src/app/shared/dishes.ts
@@ -140,7 +140,7 @@ export const DISHES: Dish[] = [
         comments: [
             {
                 rating: 5,
-                comment: 'Imagine all the eatables, living in conFusion!',
+                comment: 'Imagine all the eatables, living in conFusion!!!',
                 author: 'John Lemon',
                 date: '2012-10-16T17:57:28.556094Z'
             },
